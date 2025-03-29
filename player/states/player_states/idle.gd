85e681da7fd669b93bfa17fce4b02c35cdc8627d
Ke Duong <kelyduong@gmail.com>
2025-03-29 15:07:30 +1100

    fix idle problem of not moving when click multiple arrow keys

--- player/states/player_states/idle.gd
+++ player/states/player_states/idle.gd
@@ -4,10 +4,7 @@ func enter(_previous_state_path: String, _data:={}) -> void:
 	player.velocity = Vector2.ZERO
 
 func physics_update(_delta: float) -> void:
-	if Input.is_action_just_pressed("move_up") \
-		or Input.is_action_just_pressed("move_down") \
-		or Input.is_action_just_pressed("move_left") \
-		or Input.is_action_pressed("move_right"):
+	if Input.get_vector("move_left", "move_right", "move_up", "move_down") != Vector2.ZERO:
 			finished.emit(RUNNING)
 	elif Input.is_action_just_pressed("attack"):
 		finished.emit(ATTACKING)
